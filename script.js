ac83b3053a3e76b55e17ac0088d6d1d31a808fa4
Arthur Polimeni <arthurpolimeni17@gmail.com>
2023-08-03 11:47:14 -0300

    Adicionando historico

--- script.js
+++ script.js
@@ -18,10 +18,16 @@
 // OU
 
 function verificar() {
-  const sexo = document.querySelector("input[name='sexo']:checked").value;
-  const numbers = document.querySelector("input#value");
+  const sexo = document.querySelector("input[name='sexo']:checked")?.value;
+  const idade = Number(document.querySelector("input#idade").value);
+  const nome = document.querySelector("input#nome").value;
+
+  if (!sexo) {
+    alert("Preencha os campos corretamente");
+    return;
+  }
+
   const res = document.querySelector("div.res");
-  const idade = Number(numbers.value);
   let img = document.createElement("img");
   img.classList.add("foto-pessoa");
   res.innerText = getTexto(idade, sexo);
@@ -30,6 +36,9 @@ function verificar() {
   if (imagemContainer) container.removeChild(imagemContainer);
   img.setAttribute("src", getImageUrl(idade, sexo));
   container.appendChild(img);
+  inserirNoLocalStorage(nome, idade, sexo);
+  limparCampos();
+  inserirLinha(nome, idade, sexo);
 }
 
 function getImageUrl(idade, sexo) {
@@ -65,4 +74,48 @@ function getTexto(idade, sexo) {
     texto += `, Prioritário`;
   }
   return texto;
+}
+
+function inserirLinha(nome, idade, sexo) {
+  const tbody = document.querySelector("table.tabela-historico tbody");
+  tbody.insertAdjacentHTML(
+    "beforeend",
+    `
+    <tr>
+      <td>${nome}</td>
+      <td>${idade}</td>
+      <td>${sexo === "h" ? "Homem" : "Mulher"}</td>
+    </tr>
+  `
+  );
+}
+
+function limparCampos() {
+  const idade = document.querySelector("input#idade");
+  const nome = document.querySelector("input#nome");
+  idade.value = "";
+  nome.value = "";
+}
+
+function inserirNoLocalStorage(nome, sexo, idade) {
+  const historicoArmazenado = localStorage.getItem("historico");
+  let historico = [];
+  if (historicoArmazenado) {
+    historico = JSON.parse(historicoArmazenado);
+  }
+
+  const pessoa = {
+    nome: nome,
+    idade: idade,
+    sexo: sexo,
+  };
+  historico.push(pessoa);
+  localStorage.setItem("historico", JSON.stringify(historico));
+}
+
+function exibirDados() {
+  const historico = JSON.parse(localStorage.getItem("historico"));
+  historico.forEach(function (pessoa) {
+    inserirLinha(pessoa.nome, pessoa.idade, pessoa.sexo);
+  });
 }
